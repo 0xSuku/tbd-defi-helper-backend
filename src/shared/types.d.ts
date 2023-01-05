d503d1985354c716740d61e9b4b8f5b7ee2232c1
Suku <unaeternidad@gmail.com>
2023-01-04 21:14:26 -0300

    changes on response object

---
+++ src/shared/types.d.ts
@@ -0,0 +1,15 @@
+import { Token } from "@uniswap/sdk-core"
+
+type TokenInfo = {
+    [key: string]: TokenDetails;
+}
+
+type TokenDetails = {
+    token: Token;
+    disabled?: boolean;
+}
+
+type WalletResponse = { 
+    token: Token;
+    amount: string;
+}
