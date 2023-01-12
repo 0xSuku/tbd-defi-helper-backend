a2016a35766a9a92329937ae5bace0f128cafddd
Suku <unaeternidad@gmail.com>
2023-01-12 14:23:21 -0300

    requesting prices every 10 seconds

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require('cors');
 app.use(cors());
-fetchCoingeckoPrices();
+setInterval(() => fetchCoingeckoPrices(), 10000);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Healthy');
